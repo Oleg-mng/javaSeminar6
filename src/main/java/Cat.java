f9f89378ec6ab8dc73c29271065bd1ef5f7d9909
oleg <legik7127@gmail.com>
2022-12-04 13:10:29 +0600

    Added Dublicate of cat

--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -1,4 +1,3 @@
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -7,14 +6,15 @@ public class Cat {
     private String nickname;
     private String breed;
     private String color;
+    public static Set<Cat> cats;
 
     public Cat(double age, String nickname, String breed, String color){
         this.age = age;
         this.nickname = nickname;
         this.breed = breed;
         this.color = color;
+        cats = new HashSet();
     }
-
 
     @Override
     public String toString() {
@@ -22,18 +22,21 @@ public class Cat {
     }
 
     public static void main(String[] args) {
-        Set<Cat> cats = new HashSet();
+//        Set<Cat> cats = new HashSet();
         Cat catMy = new Cat(0.1, "Marmelad", "пятнистая", "рыже-черно-белый");
         Cat catMyFriend = new Cat(4, "Inga", "null", "серый");
+        Cat catMyDublicate = new Cat(0.1, "Marmelad", "пятнистая", "рыже-черно-белый");
         cats.add (catMy);
         cats.add (catMyFriend);
+        cats.add (catMyDublicate);
 //        catMy.age = 0.1;
 //        catMy.nickname = "Marmelad";
 //        catMy.breed = "пятнистая";
 //        catMy.color = "рыже-черно-белый";
-        System.out.println(catMy);
-        System.out.println();
-        System.out.println(catMyFriend);
+//        System.out.println(catMy);
+//        System.out.println();
+//        System.out.println(catMyFriend);
+        System.out.println(cats);
     }
 }
 
