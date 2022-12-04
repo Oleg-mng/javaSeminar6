7b5cb6c1f531a57d7ba3629c5e761d6a6c03d0b4
oleg <legik7127@gmail.com>
2022-12-04 12:55:07 +0600

    Added HashSet

--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -1,3 +1,7 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Cat {
     private double age;
     private String nickname;
@@ -11,14 +15,18 @@ public class Cat {
         this.color = color;
     }
 
+
     @Override
     public String toString() {
         return " возраст кота: " + age +"\n кличка: " + nickname +"\n порода: " + breed + "\n окрас: " + color;
     }
 
     public static void main(String[] args) {
+        Set<Cat> cats = new HashSet();
         Cat catMy = new Cat(0.1, "Marmelad", "пятнистая", "рыже-черно-белый");
         Cat catMyFriend = new Cat(4, "Inga", "null", "серый");
+        cats.add (catMy);
+        cats.add (catMyFriend);
 //        catMy.age = 0.1;
 //        catMy.nickname = "Marmelad";
 //        catMy.breed = "пятнистая";
@@ -27,6 +35,5 @@ public class Cat {
         System.out.println();
         System.out.println(catMyFriend);
     }
-
 }
 
